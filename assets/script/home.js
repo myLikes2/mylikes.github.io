da27ba5a669e713b8de11a9bda6a8f9e6de25d72
mylikes2 <mylikes2k21@gmail.com>
2021-08-20 23:25:06 +0530

    Contact Page Added with Responsiveness, Whatsapp, Mail Added

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,6 +1,24 @@
 const toggleBtn = document.querySelector('.toggle-btn-container')
 const menuButtons = document.querySelector('.buttons')
 
+
+const servicesButton = document.querySelector('#services-btn')
+const contactButton = document.querySelector('#contact-btn')
+
+//Contact Page//
+
+const homeButton = document.querySelector('#contact-pg-home-btn')
+
 function menuToggle() {
   menuButtons.classList.toggle('toggle-block-btn')
 }
+
+homeButton.addEventListener('click', () => {
+  location.href = 'https://mylikes2.github.io/mylikes.github.io/'
+})
+
+
+
+contactButton.addEventListener('click', () => {
+  location.href = 'https://mylikes2.github.io/mylikes.github.io/contact.html'
+})
